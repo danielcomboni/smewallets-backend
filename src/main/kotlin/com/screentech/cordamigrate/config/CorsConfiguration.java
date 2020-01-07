ba680d864296df0e5830421fb2ed06741b55252e
danielcomboni <comboni93@gmail.com>
2020-01-07 14:00:55 +0300

    modify cors

--- src/main/kotlin/com/screentech/cordamigrate/config/CorsConfiguration.java
+++ src/main/kotlin/com/screentech/cordamigrate/config/CorsConfiguration.java
@@ -15,7 +15,7 @@ public class CorsConfiguration {
             public void addCorsMappings(CorsRegistry registry) {
 registry.addMapping("/**")
                     .allowedOrigins(
-                        "http://localhost:3000")
+                        "http://localhost:4200")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")                .allowCredentials(true)
 ;
             }
