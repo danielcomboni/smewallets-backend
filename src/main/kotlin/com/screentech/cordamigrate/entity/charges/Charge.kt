56b86af233b27b1ea8e738a24ab6d3c921977a91
danielcomboni <comboni93@gmail.com>
2020-01-09 11:11:04 +0300

    entity classes with column definitions

--- src/main/kotlin/com/screentech/cordamigrate/entity/charges/Charge.kt
+++ src/main/kotlin/com/screentech/cordamigrate/entity/charges/Charge.kt
@@ -3,6 +3,8 @@ package com.screentech.cordamigrate.entity.charges
 import com.screentech.cordamigrate.entity.user.User
 import com.screentech.cordamigrate.entity.wallet.Wallet
 import com.screentech.cordamigrate.utility.getCurrentTimestampSQL
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
 import java.math.BigDecimal
 import java.sql.Timestamp
 import javax.persistence.*
@@ -10,18 +12,32 @@ import javax.persistence.*
 @Entity
 @Table(name = "charges")
 class Charge(
-        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id : BigDecimal,
-        @Column(name = "withholding_tax") var withHoldingTax : BigDecimal,
-        @Column(name = "discount_amount") var discountAmount : BigDecimal,
-        @Column(name = "transaction_fees_supplier") var transactionFeesSupplier : BigDecimal,
-        @Column(name = "excise_duty_supplier") var exciseDutySupplier : BigDecimal,
-        @Column(name = "transaction_fees_sponsor") var transactionFeesSponsor: BigDecimal,
-        @Column(name = "excise_duty_sponsor") var exciseDutySponsor: BigDecimal,
-        @Column(name = "cbr") var cbr : BigDecimal,
-        @Column(name = "interest_rate") var interestRate : BigDecimal,
-        @Column(name = "the_timestamp") var timestamp: Timestamp = getCurrentTimestampSQL(),
-        @OneToOne(cascade = arrayOf(CascadeType.PERSIST)) @JoinColumn(name = "user_id") var user: User,
-        @OneToOne(cascade = arrayOf(CascadeType.PERSIST)) @JoinColumn(name = "wallet_id") var wallet: Wallet,
+
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)  @Column(name = "id") var id : Long,
+
+        @Column(name = "withholding_tax", columnDefinition = "numeric(16,4)") var withHoldingTax : BigDecimal,
+
+        @Column(name = "discount_amount", columnDefinition = "numeric(16,4)" ) var discountAmount : BigDecimal,
+
+        @Column(name = "transaction_fees_supplier", columnDefinition = "numeric(16,4)") var transactionFeesSupplier : BigDecimal,
+
+        @Column(name = "excise_duty_supplier", columnDefinition = "numeric(16,4)") var exciseDutySupplier : BigDecimal,
+
+        @Column(name = "transaction_fees_sponsor", columnDefinition = "numeric(16,4)") var transactionFeesSponsor: BigDecimal,
+
+        @Column(name = "excise_duty_sponsor", columnDefinition = "numeric(16,4)") var exciseDutySponsor: BigDecimal,
+
+        @Column(name = "cbr", columnDefinition = "numeric(16,4)") var cbr : BigDecimal,
+
+        @Column(name = "interest_rate", columnDefinition = "numeric(16,4)") var interestRate : BigDecimal,
+
+        @Column(name = "the_timestamp", columnDefinition = "timestamp default now()") var timestamp: Timestamp? = getCurrentTimestampSQL(),
+
+        @OneToOne(cascade = arrayOf(CascadeType.PERSIST)) @OnDelete(action = OnDeleteAction.NO_ACTION) @JoinColumn(name = "user_id") var user: User,
+
+        @OneToOne(cascade = arrayOf(CascadeType.PERSIST)) @OnDelete(action = OnDeleteAction.NO_ACTION) @JoinColumn(name = "wallet_id") var wallet: Wallet,
+
         @Transient var timestampStr : String?
 
-)
+) {
+}
--- src/main/kotlin/com/screentech/cordamigrate/entity/securityquestion/SecurityQuestion.kt
+++ src/main/kotlin/com/screentech/cordamigrate/entity/securityquestion/SecurityQuestion.kt
@@ -1,25 +1,19 @@
 package com.screentech.cordamigrate.entity.securityquestion
 
 import java.sql.Date
+import java.sql.Timestamp
 import javax.persistence.*
 
 @Entity
 @Table(name = "security_questions")
-class SecurityQuestion {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    var id: Long? = null
-    @Column(name = "question")
-    var question: String? = null
-    @Column(name = "the_timestamp", updatable = true, insertable = true)
-    var timestamp: Date? = null
+class SecurityQuestion(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id") var id: Long?,
+        @Column(name = "question", columnDefinition = "text") var question: String?,
+        @Column(name = "the_timestamp", columnDefinition = "timestamp default now()") var timestamp: Timestamp?,
+        @Transient var timestampStr : String?
+) {
 
     override fun toString(): String {
-        return "SecurityQuestion{" +
-                "id=" + id +
-                ", question='" + question + '\'' +
-                ", timestamp=" + timestamp +
-                '}'
+        return "SecurityQuestion(id=$id, question=$question, timestamp=$timestamp, timestampStr=$timestampStr)"
     }
 }
