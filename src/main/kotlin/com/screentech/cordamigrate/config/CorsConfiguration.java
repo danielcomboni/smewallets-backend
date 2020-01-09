420f7959328a5e17bca0309bee31066adc90f36b
danielcomboni <comboni93@gmail.com>
2020-01-09 11:14:25 +0300

    set cors to accept calls from localhost:4200

--- src/main/kotlin/com/screentech/cordamigrate/config/CorsConfiguration.java
+++ src/main/kotlin/com/screentech/cordamigrate/config/CorsConfiguration.java
@@ -13,11 +13,10 @@ public class CorsConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-registry.addMapping("/**")
-                    .allowedOrigins(
-                        "http://localhost:4200")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")                .allowCredentials(true)
-;
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD").allowCredentials(true)
+                ;
             }
         };
     }
