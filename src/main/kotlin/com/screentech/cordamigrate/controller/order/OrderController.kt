90c3733d42e4296223170edc23f3bcab68e99a44
danielcomboni <comboni93@gmail.com>
2020-01-13 15:29:14 +0300

    order controller timestamp

--- src/main/kotlin/com/screentech/cordamigrate/controller/order/OrderController.kt
+++ src/main/kotlin/com/screentech/cordamigrate/controller/order/OrderController.kt
@@ -4,6 +4,8 @@ import com.screentech.cordamigrate.dao.order.OrderRepository
 import com.screentech.cordamigrate.entity.order.Order
 import com.screentech.cordamigrate.utility.CRUDAbstract
 import com.screentech.cordamigrate.utility.JSONUtilsKT
+import com.screentech.cordamigrate.utility.getCurrentTimestampSQL
+import com.screentech.cordamigrate.utility.parseStringToTimestamp
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -17,7 +19,12 @@ class OrderController : CRUDAbstract<Order>(){
     lateinit var orderRepository: OrderRepository
 
     @PostMapping("/create")
-    override fun create(@RequestBody anObject: Order): ResponseEntity<*> = JSONUtilsKT.ok(this.orderRepository.save(anObject))
+    override fun create(@RequestBody anObject: Order): ResponseEntity<*> {
+        anObject.buyer.emailVerifiedAt = parseStringToTimestamp(anObject.buyer.emailVerifiedAtStr)
+        anObject.supplier.emailVerifiedAt = parseStringToTimestamp(anObject.supplier.emailVerifiedAtStr)
+        anObject.timestamp = getCurrentTimestampSQL()
+      return JSONUtilsKT.ok(this.orderRepository.save(anObject))
+    }
 
     @PutMapping("/update")
     override fun update(@RequestBody anObject: Order): ResponseEntity<*> = JSONUtilsKT.ok(this.orderRepository.save(anObject))
