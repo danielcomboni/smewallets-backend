a672189d8e57a8268ec476e8597d4dc688c6b790
Daniel Comboni <42601494+danielcomboni@users.noreply.github.com>
2020-01-17 10:31:31 +0300

    Update CorsConfiguration.java

--- src/main/kotlin/com/screentech/cordamigrate/config/CorsConfiguration.java
+++ src/main/kotlin/com/screentech/cordamigrate/config/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://objective-swirles-0b6238.netlify.com")
+                        .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD").allowCredentials(true)
                 ;
             }
